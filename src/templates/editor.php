39d0ef9aa4ad982a2ca381868af47d1db347c772
Potted <ilorn.mc@gmail.com>
2021-09-19 15:09:21 +0800

    Add nav template

--- src/templates/editor.php
+++ src/templates/editor.php
@@ -1,24 +1,7 @@
 <?php $this->layout('layout') ?>
 
 <div class="flex flex-column">
-    <div class="flex-auto flex flex-row header nav">
-        <div class="bold mr">管理</div>
-        <div class="mr">
-            <a href="/timeline">公共时间轴</a>
-        </div>
-        <div class="mr">
-            <a href="/notifications">通知</a>
-        </div>
-        <div class="mr">
-            <a href="/editor">嘟嘟</a>
-        </div>
-        <div class="mr">
-            <a href="/web/following">关系</a>
-        </div>
-        <div>
-            <a href="/">返回</a>
-        </div>
-    </div>
+    <?php $this->insert('includes/nav')?>
     <div  class="flex-grow mt">
         <form  action="/notes" method="POST">
             <div class="mb">
---
+++ src/templates/includes/nav.php
@@ -0,0 +1,21 @@
+<div class="flex-auto flex flex-row header nav">
+    <div class="bold mr">管理</div>
+    <div class="mr">
+        <a href="/timeline">时间轴</a>
+    </div>
+    <div class="mr">
+        <a href="/notifications">通知</a>
+    </div>
+    <div class="mr">
+        <a href="/editor">嘟嘟</a>
+    </div>
+    <div class="mr">
+        <a href="/web/following">关系</a>
+    </div>
+    <div class="mr">
+        <a href="/settings/profile">资料</a>
+    </div>
+    <div>
+        <a href="/">返回</a>
+    </div>
+</div>
