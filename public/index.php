e4e8f279723b4c61d57ca77a84ff0b9c4793747e
Potted <ilorn.mc@gmail.com>
2021-10-04 23:25:07 +0800

    Define new constants: CHERRY_VERSION and CHERRY_REPOSITORY

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ use Slim\Factory\AppFactory;
 use DI\Container;
 
 define('ROOT', dirname(__DIR__));
+define('CHERRY_VERSION', "0.1.0");
+define('CHERRY_REPOSITORY', 'https://github.com/zither/cherry');
 
 require ROOT . '/vendor/autoload.php';
 
