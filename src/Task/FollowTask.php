4b714612767a7abec9317798a2cce058c3fbc109
Potted <ilorn.mc@gmail.com>
2021-10-07 15:28:41 +0800

    Fix missing db error

--- src/Task/FollowTask.php
+++ src/Task/FollowTask.php
@@ -27,13 +27,13 @@ class FollowTask implements TaskInterface
     {
         try {
             $account = $args['account'];
+            $db = $this->container->get(Medoo::class);
             if ($args['is_url']) {
                 $profileUrl = $account;
             } else {
                 $accountArr = explode('@', $account);
                 $webFingerUrl = sprintf('https://%s/.well-known/webfinger?resource=acct:%s', $accountArr[1], $account);
 
-                $db = $this->container->get(Medoo::class);
                 $signHttp = $this->container->get(SignRequest::class);
 
                 $webFingerRequest = new Request('GET', $webFingerUrl, [
