44d1930aae2cb712e0fa4fab209000c069c69d85
Potted <ilorn.mc@gmail.com>
2021-10-09 15:00:03 +0800

    Enhance markdown parser

--- src/Markdown.php
+++ src/Markdown.php
@@ -13,7 +13,35 @@ class Markdown extends Parsedown
 
     protected $tags = [];
 
+    public function __construct()
+    {
+        $this->InlineTypes['#'][] = 'HashTag';
+        $this->inlineMarkerList .= '#';
+    }
+
+    protected function inlineHashTag($excerpt)
+    {
+        if (preg_match('/#([^#<\s]+)/', $excerpt['text'], $matches)) {
+            $this->tags[] = $matches[1];
+            return [
+                'extent' => strlen($matches[0]),
+                'element' => [
+                    'name' => 'a',
+                    'text' => $matches[0],
+                    'attributes' => [
+                        'class' => 'mention hashtag',
+                        'href' => sprintf('https://%s/tags/%s', $this->host, $matches[1]),
+                        'rel' => 'tag'
+                    ]
+                ]
+            ];
+        }
+        return null;
+    }
+
     /**
+     * Block Atx Header elements
+     *
      * @param $Line
      * @return array|void
      */
@@ -22,18 +50,16 @@ class Markdown extends Parsedown
         return;
     }
 
-    public function text($text)
+    /**
+     * Block Setext Header elements
+     *
+     * @param $Line
+     * @param array|null $Block
+     * @return array|void|null
+     */
+    protected function blockSetextHeader($Line, array $Block = null)
     {
-        $marked = parent::text($text);
-        $pattern = '/#([^#<\s]+)/';
-        preg_match_all($pattern, $marked, $matches);
-
-        if (empty($matches[1])) {
-            return $marked;
-        }
-        $this->tags = $matches[1];
-        $replacement = sprintf('<a class="mention hashtag" href="https://%s/tags/${1}" rel="tag">#${1}</a>', $this->host);
-        return preg_replace('/#([^#<\s]+)/', $replacement, $marked);
+        return;
     }
 
     public function hashTags()
--- tests/MarkdownTest.php
+++ tests/MarkdownTest.php
@@ -11,7 +11,7 @@ class MarkdownTest extends TestCase
     {
         $tag = 'tag';
         $markup = '#' . $tag;
-        $host = 'chokecherry.cc';
+        $host = 'cherry.test';
         $target = sprintf(
             '<p><a class="mention hashtag" href="https://%s/tags/%s" rel="tag">%s</a></p>',
             $host,
@@ -22,5 +22,20 @@ class MarkdownTest extends TestCase
         $parser->setTagHost($host);
         $html = $parser->text($markup);
         $this->assertEquals($target, $html);
+    }
+
+    public function testInlineHashTag()
+    {
+        $host = 'cherry.test';
+        $markup = '标签测试 #cherry some chars here.';
+        $parser = new Markdown();
+        $parser->setTagHost($host);
+        $html = $parser->text($markup);
+        $expected = sprintf(
+            '<p>标签测试 <a class="mention hashtag" href="https://%s/tags/cherry" rel="tag">#cherry</a> some chars here.</p>',
+            $host
+        );
+        $this->assertEquals($expected, $html);
+        $this->assertEquals(['cherry'], $parser->hashTags());
     }
 }
