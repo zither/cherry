13f5f40d4adad571abf7642df370e10450b15e19
Potted <ilorn.mc@gmail.com>
2021-10-30 14:53:15 +0800

    Add ActivityPub class

---
+++ app/ActivityPub/ActivityPub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cherry\ActivityPub;
+
+class ActivityPub
+{
+    const OBJECT_TYPE_NOTE = 'Note';
+    const OBJECT_TYPE_QUESTION = 'Question';
+    const OBJECT_TYPE_ARTICLE = 'Article';
+}
