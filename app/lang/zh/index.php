b52bd01d0cd0a88c5dc3a7bf9041a672bce503f6
Potted <ilorn.mc@gmail.com>
2021-10-31 21:57:24 +0800

    Update chinese lang

--- app/lang/zh/index.php
+++ app/lang/zh/index.php
@@ -5,6 +5,6 @@ $common = require 'includes/list_common.php';
 return array_merge([
     'index_admin' => '管理',
     'activity_count' => '%d 条嘟文',
-    'following_count' => '%d 个关注',
-    'follower_count' => '%d 个关注者',
+    'following_count' => '关注了 %d 人',
+    'follower_count' => '被 %d 人关注',
 ], $common);
