8ec4642a1679056fe2c1766baecf92d94e9d0edc
Potted <ilorn.mc@gmail.com>
2021-11-01 12:26:14 +0800

    Handle SIGINT in run_tasks.sh

--- bin/run_tasks.sh
+++ bin/run_tasks.sh
@@ -1,18 +1,35 @@
  #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
 PARENT_DIR="$( dirname ${DIR} )"
-SNOOZE=5
+SNOOZE=2
 COMMAND="php $PARENT_DIR/public/index.php CronTask"
 LOG="$PARENT_DIR/storage/tasks.log"
+RUN_NEXT_TASK=true
+
+function log ()
+{
+  if [ ! -z "$1" ];
+  then
+   echo $(date '+%Y-%m-%d %H:%M:%S') $1 >> ${LOG} 2>&1
+  fi
+}
+
+function sigint_handler ()
+{
+  echo -e "\b\bTask runner will exit after current task is done."
+  RUN_NEXT_TASK=false
+}
+trap sigint_handler 2
 
 if [ -f $LOG ]; then
   touch $LOG
 fi
 
-echo `date` "starting..." >> ${LOG} 2>&1
-while true
+echo "Task runner is starting..."
+while $RUN_NEXT_TASK
 do
- ${COMMAND} >> ${LOG} 2>&1
- #echo `date` "sleeping..." >> ${LOG} 2>&1
+ output=$($COMMAND 2>&1)
+ log "$output"
  sleep ${SNOOZE}
 done
+echo "Task runner Stopped."
