0ae259c0dd7d8523411a3041507ab88e29161fc7
Potted <ilorn.mc@gmail.com>
2021-11-02 10:44:48 +0800

    Try to fetch parent object if not exist

--- app/Task/CreateRequestTask.php
+++ app/Task/CreateRequestTask.php
@@ -2,11 +2,11 @@
 
 namespace Cherry\Task;
 
+use PDOException;
+use InvalidArgumentException;
 use adrianfalleiro\FailedTaskException;
 use adrianfalleiro\TaskInterface;
 use Cherry\ActivityPub\ActivityPub;
-use Cherry\ActivityPub\ObjectType;
-use Cherry\Helper\Time;
 use Psr\Container\ContainerInterface;
 use Medoo\Medoo;
 
@@ -25,140 +25,30 @@ class CreateRequestTask implements TaskInterface
         $activityId = $args['activity_id'];
         $activity = $db->get('activities', '*', ['id' => $activityId]);
         if (empty($activity) || strtolower($activity['type']) !== 'create') {
-            throw new \InvalidArgumentException('Invalid activity type');
+            throw new InvalidArgumentException('Invalid activity type');
         }
         $rawActivity = json_decode($activity['raw'], true);
-        $object = ObjectType::createFromArray($rawActivity['object']);
-        $profile = $db->get('profiles', ['id'], ['actor' => $object->attributedTo]);
-        if (empty($profile)) {
-            $subTask = new FetchProfileTask($this->container);
-            $profile = $subTask->command(['actor' => $object->attributedTo]);
-        }
-        $originId = 0;
-        $parentId = 0;
-        if ($object->isReply()) {
-            $parent = $db->get('objects', ['id', 'origin_id', 'parent_id'], ['raw_object_id' => $object->inReplyTo]);
-            if (!empty($parent)) {
-                $originId = $parent['origin_id'] ?: $parent['id'];
-                $parentId = $parent['id'];
-            }
-        }
-
-        $poll = [];
-        if ($object->type === ActivityPub::OBJECT_TYPE_QUESTION) {
-            $multiple = isset($rawActivity['object']['anyOf']) ? true : false;
-            $choicesKey = $multiple ? 'anyOf' : 'oneOf';
-            $choices = [];
-            foreach ($rawActivity['object'][$choicesKey] as $v) {
-                $choices[] = [
-                    'type' => $v['type'],
-                    'name' => $v['name'],
-                    'count' => $v['replies']['totalItems'] ?? 0,
-                ];
-            }
-            $poll = [
-                'activity_id' => $activityId,
-                'choices' => json_encode($choices, JSON_UNESCAPED_UNICODE),
-                'end_time' => Time::UTCToLocalTime($rawActivity['object']['endTime']),
-                'voters_count' => $rawActivity['object']['votersCount'] ?? 0,
-                'multiple' => $multiple,
-            ];
-        }
+        $processObjectTask = new FetchObjectTask($this->container);
+        $objectData = $processObjectTask->process($rawActivity['object']);
+        $objectId = $objectData['id'];
 
         try {
             $db->pdo->beginTransaction();
-            $tags = [];
-            $emojis = [];
-            foreach ($object->tag as $v) {
-                if ($v['type'] === 'Hashtag') {
-                    $tags[] = trim($v['name'], '#');
-                } else if ($v['type'] === 'Emoji' && isset($v['icon']['url'])) {
-                    $emojis[$v['name']] = sprintf(
-                        '<img class="emoji" src="%s" alt="%s" referrerpolicy="no-referrer" />',
-                        $v['icon']['url'],
-                        $v['name']
-                    );
-                }
+            $updatedData = [
+                'object_id' => $objectId,
+                'profile_id' => $objectData['profile_id'],
+            ];
+            if ($objectData['unlisted']) {
+                $updatedData['unlisted'] = $objectData['unlisted'];
             }
-            $content = $object->getStringAttribute('content');
-            if (empty($content)) {
-                $content = $object->getStringAttribute('name');
-            }
-            if (!empty($emojis)) {
-                $content = str_replace(array_keys($emojis), array_values($emojis), $content);
+            $db->update('activities', $updatedData, ['id' => $activityId]);
+
+            if ($objectData['type'] === ActivityPub::OBJECT_TYPE_QUESTION) {
+                $db->update('polls', ['activity_id' => $activityId], ['object_id' => $objectId]);
             }
 
-            $unlisted = 0;
-            if ($object->isReply() && empty($object->content) && !empty($object->name)) {
-                $unlisted = 1;
-            }
-
-            $db->insert('objects', [
-                'profile_id' => $profile['id'],
-                'origin_id' => $originId,
-                'parent_id' => $parentId,
-                'raw_object_id' => $object->id,
-                'type' => $object->type,
-                'content' => $content,
-                'summary' => $object->getStringAttribute('summary'),
-                'url' => $object->getStringAttribute('url') ?: $object->id,
-                'published' => Time::UTCToLocalTime($object->published),
-                'is_local' => 0,
-                'is_public' => $object->isPublic(),
-                'is_boosted' => 0,
-                'is_sensitive' => isset($object->sensitive) && $object->sensitive,
-                'unlisted' => $unlisted
-            ]);
-            $objectId = $db->id();
-
-            if ($unlisted) {
-                $db->update('activities', ['unlisted' => $unlisted], ['id' => $activityId]);
-            }
-
-            if (!empty($poll)) {
-                $poll['object_id'] = $objectId;
-                $db->insert('polls', $poll);
-            }
-
-            if (!empty($tags)) {
-                $hashTags = [];
-                foreach ($tags as $v) {
-                    $hashTags[] = [
-                        'term' => $v,
-                        'object_id' => $objectId,
-                        'profile_id' => $profile['id'],
-                    ];
-                }
-                $db->insert('tags', $hashTags);
-            }
-
-            if (!empty($object->attachment)) {
-                $attachments = [];
-                foreach ($object->attachment as $v) {
-                    $attachments[] = [
-                        'profile_id' => $profile['id'],
-                        'object_id' => $objectId,
-                        'type' => $v['type'] ?? '',
-                        'media_type' => $v['mediaType'] ?? '',
-                        'url' => $v['url'] ?? '',
-                        'name' => $v['name'] ?? '',
-                        'hash' => $v['blurhash'] ?? '',
-                    ];
-                }
-                if (!empty($attachments)) {
-                    $db->insert('attachments', $attachments);
-                }
-            }
-
-            $db->update('activities', [
-                'object_id' => $objectId, 'profile_id' => $profile['id']
-            ], ['id' => $activityId]);
-
-            if ($object->isReply() && $parentId) {
-                $db->update('objects', ['replies[+]' => 1], ['id' => $parentId]);
-            }
             $db->pdo->commit();
-        } catch (\PDOException $e) {
+        } catch (PDOException $e) {
             $db->pdo->rollBack();
             throw new FailedTaskException($e->getMessage());
         }
--- app/Task/FetchObjectTask.php
+++ app/Task/FetchObjectTask.php
@@ -2,7 +2,12 @@
 
 namespace Cherry\Task;
 
+use PDOException;
+use Exception;
+use RuntimeException;
+use adrianfalleiro\FailedTaskException;
 use adrianfalleiro\TaskInterface;
+use Cherry\ActivityPub\ActivityPub;
 use Cherry\ActivityPub\ObjectType;
 use Cherry\Helper\SignRequest;
 use Cherry\Helper\Time;
@@ -22,89 +27,171 @@ class FetchObjectTask implements TaskInterface
 
     public function command(array $args)
     {
-        $db = $this->container->get(Medoo::class);
         $helper = $this->container->get(SignRequest::class);
         $request = new Request('GET', $args['id'], [
             'Accept' => 'application/activity+json',
             'Content-Type' => 'text/html',
         ]);
         $request = $helper->sign($request);
-
         $client = new Client();
         $response = $client->send($request);
 
+        $statusCode = $response->getStatusCode();
+        if ($statusCode === 401 || $statusCode === 404) {
+            return [];
+        }
+        if ($statusCode >= 300) {
+            throw new RuntimeException('Failed to fetch the object: ' . $args['id']);
+        }
         $data = $response->getBody()->getContents();
         $objectArr = json_decode($data, true);
-        $object = ObjectType::createFromArray($objectArr);
-
-        $profile = $db->get('profiles', ['id'], ['actor' => $object->attributedTo]);
-        if (empty($profile)) {
-            $fetchTask = new FetchProfileTask($this->container);
-            $profile = $fetchTask->command(['actor' => $object->attributedTo]);
+        if (empty($objectArr) || empty($objectArr['id'])) {
+            throw new RuntimeException('Invalid object: ' . $args['id']);
         }
 
-        // 这里没有递归
+        return $this->process($objectArr);
+    }
+
+    public function process(array $args)
+    {
+        $db = $this->container->get(Medoo::class);
+        $object = ObjectType::createFromArray($args);
+        $profile = $db->get('profiles', ['id'], ['actor' => $object->attributedTo]);
+        if (empty($profile)) {
+            $subTask = new FetchProfileTask($this->container);
+            $profile = $subTask->command(['actor' => $object->attributedTo]);
+        }
         $originId = 0;
         $parentId = 0;
         if ($object->isReply()) {
             $parent = $db->get('objects', ['id', 'origin_id', 'parent_id'], ['raw_object_id' => $object->inReplyTo]);
+            if (empty($parent)) {
+                // Just try one time to fetch parent object
+                try {
+                    $fetchParentTask = new FetchObjectTask($this->container);
+                    $parent = $fetchParentTask->command(['id' => $object->inReplyTo]);
+                } catch (Exception $e) {
+                    $parent = [];
+                }
+            }
             if (!empty($parent)) {
                 $originId = $parent['origin_id'] ?: $parent['id'];
                 $parentId = $parent['id'];
             }
         }
 
-        $newObject = [
-            'profile_id' => $profile['id'],
-            'origin_id' => $originId,
-            'parent_id' => $parentId,
-            'raw_object_id' => $object->id,
-            'type' => $object->type,
-            'content' => $object->getStringAttribute('content'),
-            'summary' => $object->getStringAttribute('summary'),
-            'url' => $object->getStringAttribute('url') ?: $object->id,
-            'published' => Time::UTCToLocalTime($object->published),
-            'is_local' => 0, // local object always exists, so only remote object goes here
-            'is_public' => $object->isPublic(),
-            'is_sensitive' => isset($object->sensitive) && $object->sensitive,
-        ];
+        $poll = [];
+        if ($object->type === ActivityPub::OBJECT_TYPE_QUESTION) {
+            $multiple = isset($args['object']['anyOf']) ? true : false;
+            $choicesKey = $multiple ? 'anyOf' : 'oneOf';
+            $choices = [];
+            foreach ($args[$choicesKey] as $v) {
+                $choices[] = [
+                    'type' => $v['type'],
+                    'name' => $v['name'],
+                    'count' => $v['replies']['totalItems'] ?? 0,
+                ];
+            }
+            $poll = [
+                'activity_id' => 0,
+                'choices' => json_encode($choices, JSON_UNESCAPED_UNICODE),
+                'end_time' => Time::UTCToLocalTime($args['endTime']),
+                'voters_count' => $rawActivity['object']['votersCount'] ?? 0,
+                'multiple' => $multiple,
+            ];
+        }
 
-        $db->insert('objects', $newObject);
-        $newObject['id'] = $db->id();
-
-        if (!empty($object->tag)) {
+        try {
+            $db->pdo->beginTransaction();
             $tags = [];
+            $emojis = [];
             foreach ($object->tag as $v) {
-                if ($v['type'] !== 'Hashtag') {
-                    continue;
+                if ($v['type'] === 'Hashtag') {
+                    $tags[] = trim($v['name'], '#');
+                } else if ($v['type'] === 'Emoji' && isset($v['icon']['url'])) {
+                    $emojis[$v['name']] = sprintf(
+                        '<img class="emoji" src="%s" alt="%s" referrerpolicy="no-referrer" />',
+                        $v['icon']['url'],
+                        $v['name']
+                    );
                 }
-                $tags[] = [
-                    'term' => trim($v['name'], '#'),
-                    'object_id' => $newObject['id'],
-                    'profile_id' => $profile['id'],
-                ];
             }
-            $db->insert('tags', $tags);
-        }
+            $content = $object->getStringAttribute('content');
+            if (empty($content)) {
+                $content = $object->getStringAttribute('name');
+            }
+            if (!empty($emojis)) {
+                $content = str_replace(array_keys($emojis), array_values($emojis), $content);
+            }
 
-        if (!empty($object->attachment)) {
-            $attachments = [];
-            foreach ($object->attachment as $v) {
-                $attachments[] = [
-                    'profile_id' => $profile['id'],
-                    'object_id' => $newObject['id'],
-                    'type' => $v['type'] ?? '',
-                    'media_type' => $v['mediaType'] ?? '',
-                    'url' => $v['url'] ?? '',
-                    'name' => $v['name'] ?? '',
-                    'hash' => $v['blurhash'] ?? '',
-                ];
+            $unlisted = 0;
+            if ($object->isReply() && empty($object->content) && !empty($object->name)) {
+                $unlisted = 1;
             }
-            if (!empty($attachments)) {
-                $db->insert('attachments', $attachments);
-            }
-        }
 
-        return $newObject;
+            $objectData = [
+                'profile_id' => $profile['id'],
+                'origin_id' => $originId,
+                'parent_id' => $parentId,
+                'raw_object_id' => $object->id,
+                'type' => $object->type,
+                'content' => $content,
+                'summary' => $object->getStringAttribute('summary'),
+                'url' => $object->getStringAttribute('url') ?: $object->id,
+                'published' => Time::UTCToLocalTime($object->published),
+                'is_local' => 0,
+                'is_public' => $object->isPublic(),
+                'is_boosted' => 0,
+                'is_sensitive' => isset($object->sensitive) && $object->sensitive,
+                'unlisted' => $unlisted
+            ];
+            $db->insert('objects', $objectData);
+            $objectData['id'] = $objectId = $db->id();
+
+            if (!empty($poll)) {
+                $poll['object_id'] = $objectId;
+                $db->insert('polls', $poll);
+            }
+
+            if (!empty($tags)) {
+                $hashTags = [];
+                foreach ($tags as $v) {
+                    $hashTags[] = [
+                        'term' => $v,
+                        'object_id' => $objectId,
+                        'profile_id' => $profile['id'],
+                    ];
+                }
+                $db->insert('tags', $hashTags);
+            }
+
+            if (!empty($object->attachment)) {
+                $attachments = [];
+                foreach ($object->attachment as $v) {
+                    $attachments[] = [
+                        'profile_id' => $profile['id'],
+                        'object_id' => $objectId,
+                        'type' => $v['type'] ?? '',
+                        'media_type' => $v['mediaType'] ?? '',
+                        'url' => $v['url'] ?? '',
+                        'name' => $v['name'] ?? '',
+                        'hash' => $v['blurhash'] ?? '',
+                    ];
+                }
+                if (!empty($attachments)) {
+                    $db->insert('attachments', $attachments);
+                }
+            }
+
+            if ($object->isReply() && $parentId) {
+                $db->update('objects', ['replies[+]' => 1], ['id' => $parentId]);
+            }
+            $db->pdo->commit();
+
+            return $objectData;
+        } catch (PDOException $e) {
+            $db->pdo->rollBack();
+            throw new FailedTaskException($e->getMessage());
+        }
     }
 }
