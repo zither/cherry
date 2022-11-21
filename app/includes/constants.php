1a93dc0455e262c73b3979ea382108d3dec66f31
Potted <ilorn.mc@gmail.com>
2022-11-21 11:12:45 +0800

    Add CHERRY_STYLE_VERSION constant

--- app/includes/constants.php
+++ app/includes/constants.php
@@ -1,5 +1,6 @@
 <?php
 
 define('CHERRY_VERSION', "0.1.21");
+define('CHERRY_STYLE_VERSION', "0.1.1");
 define('CHERRY_REPOSITORY', 'https://github.com/zither/cherry');
 define('CHERRY_ADMIN_PROFILE_ID', 1);
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8" content="width=device-width,initial-scale=1,user-scalable=no" name="viewport">
     <title><?=empty($profile) ? 'ActivityPub' : ($profile['name'] ?: $profile['preferred_name'])?></title>
-    <link rel="stylesheet" href="/assets/css/app.css?v=<?=CHERRY_VERSION?>" type="text/css">
+    <link rel="stylesheet" href="/assets/css/app.css?v=<?=CHERRY_STYLE_VERSION?>" type="text/css">
     <?php if ($this->getThemeName() !== 'default'):?>
-        <link rel="stylesheet" href="/themes/<?=$this->getThemeName()?>.css?v=<?=CHERRY_VERSION?>" type="text/css">
+        <link rel="stylesheet" href="/themes/<?=$this->getThemeName()?>.css?v=<?=CHERRY_STYLE_VERSION?>" type="text/css">
     <?php endif;?>
 </head>
 <body>
