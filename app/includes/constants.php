d0d64a3c682974e3580054f65f7ee5ff78153d46
Potted <ilorn.mc@gmail.com>
2022-11-25 09:12:05 +0800

    Update version to 0.2.0

--- app/includes/constants.php
+++ app/includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CHERRY_VERSION', "0.1.22");
+define('CHERRY_VERSION', "0.2.0");
 define('CHERRY_STYLE_VERSION', "0.1.1");
 define('CHERRY_REPOSITORY', 'https://github.com/zither/cherry');
 define('CHERRY_ADMIN_PROFILE_ID', 1);
